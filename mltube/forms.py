68196e8c005e80709b42f427acf14ca84faf2f92
yi-tech <saram1man@gmail.com>
2022-03-07 21:23:16 +0900

    first commit

---
+++ mltube/forms.py
@@ -0,0 +1,32 @@
+import os
+from django import forms
+from .models import Video,Comment
+
+class VideoForm(forms.ModelForm):
+    def clean_file(self):
+        file=self.cleaned_data.get('file',None)
+        if file:
+            extension=os.path.splitext(file.name)[-1].lower()
+            if extension not in ('.mp4','.avi'):
+                raise forms.ValidationError('동영상파일을 등록해 주세요')
+        return file    
+    
+    class Meta:
+        model=Video
+        fields=[
+            'title',
+            'description',
+            'file',
+            'photo',
+        ]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields= [
+            'content',
+        ]
+        widgets = {
+            'content' : forms.Textarea(attrs={'rows':3}),
+       }
---
+++ mltube/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path
+
+app_name='mltube'
+urlpatterns=[
+    path('',views.VideoListView.as_view() ,name='video_list'),
+    path('new',views.VideoCreateView.as_view() ,name='video_new'),
+    path('<int:pk>/',views.VideoDetailView.as_view() ,name='video_detail'),
+    path('<int:pk>/edit',views.VideoUpdateView.as_view() ,name='video_edit'),
+    path('<int:pk>/delete',views.VideoDeleteView.as_view() ,name='video_delete'),
+   
+   path('<int:Video_pk>/comments/new',views.CommentCreateView.as_view() ,name='comment_new'),
+   path('<int:Video_pk>/comments/<int:pk>/delete',views.CommentDeleteView.as_view() ,name='comment_delete'),
+]
